3a94e2ad9595c378d6331c6cdd3167143cda3701
Wellingotn luiz <wellingtonluizwork@gmail.com>
2023-05-14 16:46:20 -0300

    Ajustes menu

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const Input = document.querySelector('.menu-faketrigger')
+const Nav = document.querySelector('.header-navegacao')
 
+Input.addEventListener('click', () => {
+  Nav.classList.toggle('active')
+})
